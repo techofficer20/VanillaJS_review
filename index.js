2b0ec905bbf405bc16b29d75d4164c22bea4b8cc
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-06 05:37:25 +0900

    1.9 Organizing Data with Arrays

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-/* 필요하기 전까진 let 말고 const로 쓰기 */
-/* String
-const what = "Nicolas";
-*/
-/* Boolean
-const wat = true;
-*/
-/*
-Number
-const wat = 666;
-*/
-/*
-Float
-cost wat = 55.1;
-*/
+const something = "Something";
+
+const daysOfWeek = [
+  /* 대괄호 사용 */
+  /* carmel case = 변수명 사이에 공백이 있어야 했던 것들은
+다음 문자를 대문자로 변경하는 것으로 대체 */
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat",
+  "Sun",
+  54 /* 정수형, boolean 값 있어도 상관 X */,
+  true,
+  "stuff",
+  "lalala"
+];
+
+console.log(daysOfWeek[4]); /* 0부터 시작하므로 Sat 출력됨. */
