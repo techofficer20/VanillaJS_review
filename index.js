7c9f6c0f1568ba58956f2faae0923d5ff2f50e43
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-06 05:24:34 +0900

    1.6 Your first JS Variable

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-/* alert('Im Working. Im JS. Im Beautiful. Im worth it'); */
-/* alert: 단순 경고창 */
-console.log("Im Working. Im JS. Im Beautiful. Im worth it");
-/* console.log(): 괄호 안에 써있는 것을 console이라는 부분에서 출력 */
+/* 모든 instruction은 각기 다른 라인에 위치함.
+한 expression이 끝나면 세미콜론 추가
+변수 선언 전, let을 써야 함. (초기화, 생성 시)
+이미 생성된 것을 사용할 때는 참조만 하면 됨.*/
+let a = 221;
+let b = a - 5;
+console.log(b);
