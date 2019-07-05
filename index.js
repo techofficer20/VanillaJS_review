d0cf5e062fa83b2bdf6bba57395a33cfa954b761
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-06 05:20:11 +0900

    1.5 Hello World with Javascript

---
+++ index.js
@@ -0,0 +1,4 @@
+/* alert('Im Working. Im JS. Im Beautiful. Im worth it'); */
+/* alert: 단순 경고창 */
+console.log("Im Working. Im JS. Im Beautiful. Im worth it");
+/* console.log(): 괄호 안에 써있는 것을 console이라는 부분에서 출력 */
