0795326c400aab24805e424dadd54b2289bedf82
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-06 05:31:17 +0900

    1.8 Data Types on JS

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-/* 상수 선언하려면 let 대신 const 쓰기
-let으로 선언한 뒤 또 선언하면 이미 변수 선언했었다고 알려준다.
-(var는 알려주지 않는다. 즉, var를 쓰지 말고 let을 쓰도록 한다.)*/
-let a = 221;
-let b = a - 5;
-console.log(b, a);
+/* 필요하기 전까진 let 말고 const로 쓰기 */
+/* String
+const what = "Nicolas";
+*/
+/* Boolean
+const wat = true;
+*/
+/*
+Number
+const wat = 666;
+*/
+/*
+Float
+cost wat = 55.1;
+*/
