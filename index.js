c7780dfe56e6ec478c4e42ebb3564cf1deb39ca0
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-06 05:44:22 +0900

    1.10 Organizing Data with Objects

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const something = "Something";
-
-const daysOfWeek = [
-  /* 대괄호 사용 */
-  /* carmel case = 변수명 사이에 공백이 있어야 했던 것들은
-다음 문자를 대문자로 변경하는 것으로 대체 */
-  "Mon",
-  "Tue",
-  "Wed",
-  "Thu",
-  "Fri",
-  "Sat",
-  "Sun",
-  54 /* 정수형, boolean 값 있어도 상관 X */,
-  true,
-  "stuff",
-  "lalala"
-];
-
-console.log(daysOfWeek[4]); /* 0부터 시작하므로 Sat 출력됨. */
+const nicoInfo = {
+  /* 중괄호를 쓴다. */
+  name: "Nico",
+  age: 33,
+  gender: "Male",
+  isHandsome: true,
+  favMovies: [
+    "Along the gods",
+    "LOTR",
+    "Oldboy"
+  ] /* Object 안에 Array 넣기 가능 */,
+  favFood: [
+    /* Array 안에 Object 넣기 가능 */
+    { name: "Kimchi", fatty: false },
+    { name: "Cheese burger", fatty: true }
+  ]
+};
+console.log(nicoInfo.gender);
+nicoInfo.gender = "Female"; /* 수정 가능 */
+console.log(nicoInfo.gender);
