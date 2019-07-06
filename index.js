c31e3f0f563ca8934f950950fa8b966a83d0aefd
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-06 14:55:47 +0900

    2.3 Modifying the DOM with JS

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const title = document.getElementById("title");
+/* const title = document.getElementById("title"); */
+const title = document.querySelector("#title"); // querySelector: 노드의 첫 번째 자식을 반환.
 title.innerHTML = "Hi! From JS";
-/* title이라는 것을 객체로 가져오고, 그 객체 안에 있는 innerHTML의 key값을 변경했음. 이것이 바로 DOM. */
+title.style.color = "red";
+document.title = "I own you now";
+/* console.dir(title); */
+/* 주의 사항 */
+/* DOM은 단순한 값 대입이 아니다. C언어로 치면 포인터와 비슷하다고 생각하면 된다. (개인적인 생각으론) 
+ex) title.style.color = "red"를 쓰면, title이라는 상수에 적용이 된다기보다는
+html에서 id가 title이었던 것에 있던 color가 red로 바뀌는 거로 생각.
+*/
