a390573e857898e2652520cb3c34faba40aa9d05
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-06 14:36:03 +0900

    2.1.1 More Function Fun

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function sayHello(name, age) {
-  console.log("Hello!", name, "you have", age, "years of age");
-}
-sayHello("Nicolas", 15);
+const calculator = {
+  // Object와 같음. (중괄호)
+  plus: function(a, b) {
+    return a + b; // plus라는 key값은 function(a, b)의 리턴값, 즉 a + b
+  }
+};
+const plus = calculator.plus(5, 5);
+console.log(plus);
