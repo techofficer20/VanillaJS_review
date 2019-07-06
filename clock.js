04e92140d053ee7d21a241032d875758deab1aaa
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-06 17:02:20 +0900

    3.2 Making a JS Clock part Two

--- clock.js
+++ clock.js
@@ -1,12 +1,18 @@
 const clockContainer = document.querySelector(".js-clock"),
   clockTitle = clockContainer.querySelector("h1");
-function init() {
-  /* 시간 설정 방법
+function getTime() {
   const date = new Date();
-  new Date(); ==> 시간 정보 가져옴
   const minutes = date.getMinutes();
   const hours = date.getHours();
-  */
-  clockTitle.innerText = `${hours}:${minutes}`;
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  // 삼항 연산자.
+  // hour가 10보다 작으면 0을 붙이고, 아니면 그냥 hours만 출력하는 것처럼..
+}
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
 }
 init();
