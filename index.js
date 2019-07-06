1b5fc9dfd06f0f08303ac49a98a24cf3d7a6d582
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-06 16:30:40 +0900

    2.6 DOM - if else - Function practice

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const title = document.querySelector("#title");
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
 
 function handleClick() {
-  title.style.color = "blue";
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    // If the color is same as the initial one
+    title.style.color = OTHER_COLOR;
+  } else {
+    // If the color has changed,
+    // we make it into the initial one again
+    title.style.color = BASE_COLOR;
+  }
 }
+
+function init() {
+  title.style.color = BASE_COLOR;
+}
+
 title.addEventListener("click", handleClick);
+init();
