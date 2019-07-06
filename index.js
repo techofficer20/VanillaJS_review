7c9eefb1b26c997707fc9b8376c1d3e747ded331
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-06 14:44:26 +0900

    2.2 JS DOM Functions

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const calculator = {
-  // Object와 같음. (중괄호)
-  plus: function(a, b) {
-    return a + b; // plus라는 key값은 function(a, b)의 리턴값, 즉 a + b
-  }
-};
-const plus = calculator.plus(5, 5);
-console.log(plus);
+const title = document.getElementById("title");
+title.innerHTML = "Hi! From JS";
+/* title이라는 것을 객체로 가져오고, 그 객체 안에 있는 innerHTML의 key값을 변경했음. 이것이 바로 DOM. */
