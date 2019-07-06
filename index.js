c47515e21a3e441151b8119291baa7f7e34db0f3
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-06 14:26:08 +0900

    2.1 Your first JS Function

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const nicoInfo = {
-  /* 중괄호를 쓴다. */
-  name: "Nico",
-  age: 33,
-  gender: "Male",
-  isHandsome: true,
-  favMovies: [
-    "Along the gods",
-    "LOTR",
-    "Oldboy"
-  ] /* Object 안에 Array 넣기 가능 */,
-  favFood: [
-    /* Array 안에 Object 넣기 가능 */
-    { name: "Kimchi", fatty: false },
-    { name: "Cheese burger", fatty: true }
-  ]
-};
-console.log(nicoInfo.gender);
-nicoInfo.gender = "Female"; /* 수정 가능 */
-console.log(nicoInfo.gender);
+function sayHello(name, age) {
+  console.log("Hello!", name, "you have", age, "years of age");
+}
+sayHello("Nicolas", 15);
