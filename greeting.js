41180cafea3ae2805bbff5a1fcbbc86887568696
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-06 17:23:58 +0900

    3.4 Saving the User Name part Two

--- greeting.js
+++ greeting.js
@@ -4,7 +4,19 @@ const form = document.querySelector(".js-form"),
 
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
-
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+function handleSubmit(event) {
+  event.preventDefault(); // 기본 event 실행값을 막는다.
+  const currnetValue = input.value;
+  paintGreeting(currnetValue);
+  saveName(currnetValue);
+}
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,7 +26,7 @@ function paintGreeting(text) {
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    // she is not
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
